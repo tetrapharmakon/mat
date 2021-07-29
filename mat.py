89af57ae99b85c79df114853e69e8b8a2db93376
fouche <tetrapharmakon@gmail.com>
2021-07-29 10:55:09 +0300

    improve case handling

--- mat.py
+++ mat.py
@@ -1,22 +1,8 @@
 import sys, pyperclip
 
-# ØpbBvV
+# ØpbBvV is a matrix environment
 def is_matrix(c):
   return c in ["s", "p", "b", "B", "v", "V", ""]
-
-# def switcher(c):
-#   cases = {
-#     "":  "",
-#     "s": "small",
-#     "p": "p",
-#     "b": "b",
-#     "B": "B",
-#     "v": "v",
-#     "V": "V",
-#     "t": "tabular",
-#     "x": "xymatrix"
-#   }
-#   return cases.get(c, "Do you think this is a joke, bub?")
 
 def intersperse(iterable, delimiter):
   it = iter(iterable)
@@ -33,32 +19,30 @@ def splitter(m):
 
 def matter(matrix):
   pre = sys.argv[1]
+  entries = splitter(matrix)
+  body = gen_matrix(entries)
   if is_matrix(pre):
      # catches argv[1] containing the specific matrix env
     beg = "\\left(\\begin{smallmatrix}\n" if pre == "s" else "\\begin{" + pre + "matrix}\n"
     end = "\n\\end{smallmatrix}\\right)\n" if pre == "s" else "\n\\end{" + pre + "matrix}\n"
     # `smallmatrix` needs brackets around the env
-    entries = splitter(matrix)
-    body = gen_matrix(entries)
     tex_matrix = beg + body + end
     return tex_matrix
   elif pre[0] == "t": # is_tabular(pre)
-    entries = splitter(matrix)
     row = len(entries[0])
     beg = "\\begin{tabular}{" + row * pre[1] + "}\n"
     end = "\n\\end{tabular}\n"
-    body = gen_matrix(entries)
     tex_matrix = beg + body + end
     return tex_matrix
   elif pre[0] == "x": # is_xy(pre)
     beg = "\\xymatrix{\n"
     end = "\n}"
-    entries = splitter(matrix)
-    body = gen_matrix(entries)
-    tex_matrix = beg + body + end
-    return tex_matrix
   else:
-    return "Do you think this is a joke, bub?"
+    beg = "Do "
+    body = "you "
+    end = "think this is a joke, bub?"
+  tex_matrix = beg + body + end
+  return tex_matrix
 
 def send(message):
   pyperclip.copy(message)
